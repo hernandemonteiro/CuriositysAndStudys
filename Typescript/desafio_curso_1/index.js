5f529dd70cda20510235c4b43544b3c0f307ff3d
Hernande Monteiro <monteiro1998@live.com>
2022-11-03 14:20:36 -0300

    iniciando mais um curso de TS

---
+++ Typescript/desafio_curso_1/index.js
@@ -0,0 +1,5 @@
+function sumTwoNumbers(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+sumTwoNumbers(3, 4);
+sumTwoNumbers(3, -4);
---
+++ Typescript/desafio_curso_1/index.ts
@@ -0,0 +1,7 @@
+function sumTwoNumbers(firstNumber: number, secondNumber: number) {
+  console.log(firstNumber + secondNumber);
+}
+
+sumTwoNumbers(3, 4);
+// sumTwoNumbers(3, "4"); // generate error;
+sumTwoNumbers(3, -4); // allow negative numbers
