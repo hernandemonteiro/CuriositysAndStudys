af692d374b954f1aedf61a8b860c845d8ae270e4
Hernande Monteiro <monteiro1998@live.com>
2022-12-25 17:50:34 -0300

    study C

--- C/first_program/advinhacao.c
+++ C/first_program/advinhacao.c
@@ -2,7 +2,15 @@
 
 int main()
 {
-    printf("************************************");
-    printf("* Bem-vindo ao jogo da advinhação! *");
-    printf("************************************");
+    printf("************************************\n");
+    printf("* Bem-vindo ao jogo da advinhação! *\n");
+    printf("************************************\n");
+
+    // int numerosecreto;
+    // numerosecreto = 42;
+
+    int chute;
+    printf("Qual é seu chute? \n");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d!\n", chute);
 }
