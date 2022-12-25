c3f8b09b3be3bf70d3dcba76b95717b998cf5f22
Hernande Monteiro <monteiro1998@live.com>
2022-12-25 19:19:18 -0300

    conditionals and for loop

--- C/first_program/advinhacao.c
+++ C/first_program/advinhacao.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main()
 {
     printf("************************************\n");
     printf("* Bem-vindo ao jogo da advinhação! *\n");
     printf("************************************\n");
 
-    // int numerosecreto;
-    // numerosecreto = 42;
+    int numeroSecreto = 5;
 
-    int chute;
-    printf("Qual é seu chute? \n");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d!\n", chute);
+    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++)
+    {
+        int chute;
+        printf("Qual é seu chute? \n");
+        scanf("%d", &chute);
+
+        int acertouNumero = chute == numeroSecreto;
+        int chancesDeAcertar = NUMERO_DE_TENTATIVAS - i;
+        int numeroMaiorQueSecreto = chute > numeroSecreto;
+
+        printf("Você ainda tem %d chances de acertar!\n", chancesDeAcertar);
+
+        if (acertouNumero)
+        {
+            printf("Parabéns, Você acertou o numero!\n");
+            break;
+        }
+        else if (numeroMaiorQueSecreto)
+        {
+            printf("Seu numero é maior que o numero secreto!\n");
+        }
+        else
+        {
+            printf("Seu numero é menor que o numero secreto!\n");
+        }
+    }
 }
